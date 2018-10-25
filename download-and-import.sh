390f366d8d094941e07b3d9e8635e89ee1244402
sdilts <stuart.dilts@gmail.com>
2018-10-24 22:35:13 -0600

    Initialize the databse in download-and-import.sh

--- download-and-import.sh
+++ download-and-import.sh
@@ -2,6 +2,10 @@
 
 # download the files:
 ./get-data.sh data
+# initialize the database:
+cat create-db.sql | sqlite3 imbd.db
+
+# import the database:
 ./import-db.py data imbd.db
 # kaggle isn't automatically downloaded, so don't run this:
 # ./import-kaggle.py data imbd.db
